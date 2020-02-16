8ca7111a06c59e714a196e39f20c3a30950444f5
xudafeng <xudafeng@126.com>
2020-02-16 22:52:54 +0800

    init

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const Realm = require('leoric');
+
+const initModel = require('./model');
+
+const config = {
+  dialect: 'sqlite',
+  storage: './test.sqlite',
+  define: {
+    underscored: false,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+    deletedAt: false,
+    paranoid: false,
+  },
+  logger: {
+    logQuery(sql) {
+      console.log(sql);
+    }
+  },
+};
+
+const db = new Realm(config);
+
+const Test1 = initModel(db);
+db.models.Test1 = Test1;
+
+(async () => {
+  await db.sync();
+  const s = 's';
+  const res = await db.driver.query(`pragma key '${s}'`);
+  console.log(res);
+})();
